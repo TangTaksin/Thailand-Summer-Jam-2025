a758f5158eb3c1aeac82c5f3b0df27c7efd92637
Daggoot <taksin_tang51@hotmail.com>
2025-04-19 13:45:25 +0700

    Grid system and player movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GridManager gridManager;
+    public GameObject playerPrefab;
+    public Button startButton;
+
+    private GameObject playerObject;
+    private Tile currentTile;
+    private Tile goalTile;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        if (startButton != null)
+            startButton.onClick.AddListener(StartGame);
+    }
+
+    public void StartGame()
+    {
+        if (playerObject != null)
+            Destroy(playerObject);
+
+        int goalX = Random.Range(0, gridManager.width);
+        int goalY = gridManager.height - 1;
+        goalTile = gridManager.GetTile(goalX, goalY);
+        goalTile.Reveal();
+        goalTile.SetAsGoal();
+
+        int startX = Random.Range(0, gridManager.width);
+        int startY = 0;
+        Tile startTile = gridManager.GetTile(startX, startY);
+        startTile.Reveal();
+
+        playerObject = Instantiate(playerPrefab, startTile.transform.position + new Vector3(0, 0, -1), Quaternion.identity);
+        currentTile = startTile;
+
+        RevealAdjacentTiles(currentTile);
+    }
+
+    public bool CanReveal(Tile tile)
+    {
+        return IsAdjacent(tile, currentTile) && tile.state == Tile.TileState.Normal;
+    }
+
+    public bool CanMoveTo(Tile tile)
+    {
+        return IsAdjacent(tile, currentTile) &&
+               (tile.state == Tile.TileState.Revealed || tile.state == Tile.TileState.RevealedLocked);
+    }
+
+    public void AddRevealOption(Tile tile)
+    {
+        // You can track reveal options if needed
+    }
+
+    public void MovePlayerTo(Tile tile)
+    {
+        currentTile.LockTile();
+        currentTile = tile;
+        playerObject.transform.position = tile.transform.position + new Vector3(0, 0, -1);
+
+        if (tile == goalTile)
+        {
+            Debug.Log("🎉 You reached the goal!");
+            return;
+        }
+
+        RevealAdjacentTiles(currentTile);
+    }
+
+    void RevealAdjacentTiles(Tile tile)
+    {
+        TryReveal(tile.x + 1, tile.y);
+        TryReveal(tile.x - 1, tile.y);
+        TryReveal(tile.x, tile.y + 1);
+        TryReveal(tile.x, tile.y - 1);
+    }
+
+    void TryReveal(int x, int y)
+    {
+        Tile tile = gridManager.GetTile(x, y);
+        if (tile != null)
+            tile.Reveal();
+    }
+
+    bool IsAdjacent(Tile a, Tile b)
+    {
+        return (Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y)) == 1;
+    }
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,11 +1,77 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    void OnMouseDown()
+    public enum TileState
     {
-        Debug.Log("Clicked tile: " + gameObject.name);
-        Destroy(gameObject);
+        Normal,
+        Revealed,
+        RevealedLocked
+    }
+
+    public int x, y;
+    public TileState state = TileState.Normal;
+    private GridManager gridManager;
+    private SpriteRenderer sr;
+
+    public void Init(int x, int y, GridManager grid)
+    {
+        this.x = x;
+        this.y = y;
+        gridManager = grid;
+        sr = GetComponent<SpriteRenderer>();
+        UpdateTileAppearance();
+    }
+
+    public void UpdateTileAppearance()
+    {
+        switch (state)
+        {
+            case TileState.Normal:
+                sr.color = Color.black;
+                break;
+            case TileState.Revealed:
+                sr.color = Color.gray;
+                break;
+            case TileState.RevealedLocked:
+                sr.color = Color.yellow;
+                break;
+        }
+    }
+
+    public void Reveal()
+    {
+        if (state == TileState.Normal)
+        {
+            state = TileState.Revealed;
+            UpdateTileAppearance();
+        }
+    }
+
+    public void LockTile()
+    {
+        if (state == TileState.Revealed)
+        {
+            state = TileState.RevealedLocked;
+            UpdateTileAppearance();
+        }
+    }
+
+    public void SetAsGoal()
+    {
+        sr.color = Color.green;
+    }
+
+    private void OnMouseDown()
+    {
+        if ((state == TileState.Revealed || state == TileState.RevealedLocked) && GameManager.Instance.CanMoveTo(this))
+        {
+            GameManager.Instance.MovePlayerTo(this);
+        }
+        else if (state == TileState.Normal && GameManager.Instance.CanReveal(this))
+        {
+            Reveal();
+            GameManager.Instance.AddRevealOption(this);
+        }
     }
 }

1c46c518e59adf35b83becc7d342586cad31d851
ResNeedle <110387872+ResNeedle@users.noreply.github.com>
2025-04-25 02:16:40 +0700

    Sliding Tile

---
+++ Assets/Scripts/Tile Scripts/Tile Events/SlideEvent.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class SlideEvent : TileEvents
+{
+    GridManager gridManager;
+
+    public enum DirectionType{
+        ice,
+        up,
+        down,
+        left,
+        right
+    }
+    public DirectionType directionType;
+
+    private void Awake()
+    {
+        gridManager = GetComponentInParent<GridManager>();
+    }
+
+    public override void Effect()
+    {
+        var gameManager = GameManager.Instance;
+
+        var lastTile = gameManager.lastTile;
+        var attachTile = new Vector2Int(attachedTile.x, attachedTile.y);
+        var desPos = Vector2Int.zero;
+
+        Tile Destination = null;
+
+        switch (directionType)
+        {
+            case DirectionType.ice:
+                var direction = new Vector2Int(attachedTile.x, attachedTile.y) 
+                            - new Vector2Int(lastTile.x, lastTile.y);
+                desPos = attachTile + direction;
+
+                break;
+
+            case DirectionType.up:
+                desPos = attachTile + new Vector2Int(0, 1);
+                break;
+
+            case DirectionType.down:
+                desPos = attachTile + new Vector2Int(0, -1);
+                break;
+
+            case DirectionType.left:
+                desPos = attachTile + new Vector2Int(-1, 0);
+                break;
+
+            case DirectionType.right:
+                desPos = attachTile + new Vector2Int(1, 0);
+                break;
+        }
+
+        Destination = gridManager.GetTile(desPos.x, desPos.y);
+        if (Destination)
+        {
+            gameManager.MovePlayerTo(Destination);
+            Destination.OnEnterTile?.Invoke();
+        }
+        else
+        {
+            gameManager.MovePlayerTo(attachedTile);
+        }
+
+    }
+}
